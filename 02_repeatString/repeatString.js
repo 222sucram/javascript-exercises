76d3d722bff784384eebb4805e96cf9887ce5893
Marcus <marcus.oflaherty222@gmail.com>
2023-05-04 00:46:18 +0100

    function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let strConc = ''
+    for(let i = 0; i < num; i++) {
+        strConc += str;
+    }
+    return strConc;
 };
 
 // Do not edit below this line
-module.exports = repeatString;
+ module.exports = repeatString;
