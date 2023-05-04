6a4cd1b2afdb40134f14143207132320af63d061
Marcus <marcus.oflaherty222@gmail.com>
2023-05-04 17:19:05 +0100

    functions to convert celsius and farenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const n = (temp - 32) * 5/9
+  return Math.round(n * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const n = (temp * 9/5) + 32
+  return Math.round(n * 10) / 10
 };
 
 // Do not edit below this line
