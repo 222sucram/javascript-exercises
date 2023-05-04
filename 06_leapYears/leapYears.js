0b75fe37f3e24e6f5985f19c1b18e819a13ea84a
Marcus <marcus.oflaherty222@gmail.com>
2023-05-04 15:34:45 +0100

    calculate a if a given int is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    yearStr = '' + year
+    yearCentury = yearStr.slice(-2)
 
+    if(year % 4 === 0 && yearCentury == '00' && yearStr % 400 === 0 || year % 4 === 0 && yearCentury != '00') return true;
+
+    if(year % 4 !== 0) return false;
+
+    if(yearCentury == '00' && yearStr % 400 !== 0) return false
 };
 
 // Do not edit below this line
