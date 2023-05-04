2cf53100f3cb0711a20e4d7619c6df47b545c81c
Marcus <marcus.oflaherty222@gmail.com>
2023-05-04 13:54:25 +0100

    Algo that sums ints within a range

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,7 +3,6 @@ const removeFromArray = function(arr, ...num) {
 
    console.log(arr.filter(e => num.includes(e) ? null : outArr.push(e)))
 
-    console.log(outArr)
     // for (i of arr) {
     //     i !== num ? outArr.push(i) : null
     // }
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    const arr = new Array(...arguments).sort((a,b) => a-b);
 
+    let start = arr[0]
+    let end = arr[1]
+    let sum = 0;
+
+    if(start < 0 || end < 0) {
+        return 'ERROR'
+    }
+    
+    if(typeof start !== 'number' || typeof end !== 'number') {
+        return ('ERROR')
+    }
+
+   for (i = start; i <= end; i++) {
+    sum += i
+    start++
+   }
+    console.log(sum)
+    return sum
 };
 
 // Do not edit below this line
