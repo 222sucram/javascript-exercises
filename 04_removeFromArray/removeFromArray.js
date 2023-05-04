abc6b3cbe8f05e6842f5b29f241740b86373f0ac
Marcus <marcus.oflaherty222@gmail.com>
2023-05-04 13:06:57 +0100

    algo to remove elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    const outArr = [];
 
+   console.log(arr.filter(e => num.includes(e) ? null : outArr.push(e)))
+
+    console.log(outArr)
+    // for (i of arr) {
+    //     i !== num ? outArr.push(i) : null
+    // }
+     return outArr
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
