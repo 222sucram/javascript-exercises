d2de8b67ca0cb9d47f3d9c6fef3eca9708d440b0
Marcus <marcus.oflaherty222@gmail.com>
2023-05-12 02:21:16 +0100

    complete array method exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	 return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length === 0) return 0
+    
+    return arr.reduce((a, b) => a + b)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	 return num ** exp
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+
+  if(num === 0) {
+    return 1
+  }
+
+  for(let i = 1; i <= num; i++){
+    result *= i;
+  }
+
+  return result
 };
 
 // Do not edit below this line
