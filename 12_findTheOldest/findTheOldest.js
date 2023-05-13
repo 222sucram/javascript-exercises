73f0713a5457c8c6609edebbfc1ee76c263a74b4
Marcus <marcus.oflaherty222@gmail.com>
2023-05-13 16:35:09 +0100

    find the oldest person in a given object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    const thisYear = new Date
+
+    const living = obj.forEach((person) => !person.yearOfDeath ? person.yearOfDeath = thisYear.getFullYear() : null)
+
+    //Loop though input array and select the difference between dates, reduce arr to largest num.
+    const getOlder = obj.reduce((acc, curr) => {return curr.yearOfDeath - curr.yearOfBirth > acc.yearOfDeath - acc.yearOfBirth ? curr : acc});
+
+    return getOlder
 
 };
 
