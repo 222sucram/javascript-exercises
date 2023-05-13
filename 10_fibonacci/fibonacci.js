8decfc0d9978e99fc6c51ca78b8881eaeea8d70f
Marcus <marcus.oflaherty222@gmail.com>
2023-05-13 03:45:18 +0100

    gets given number of fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+    const seqArr = [1,1]
+    const count = typeof int === 'string' ? + int : int
+    if(int < 0) return 'OOPS'
+    for(let i = 0; i < count; i++) {
+       const newInt = seqArr[seqArr.length - 2] + seqArr[seqArr.length -1]
+       seqArr.push(newInt)
+    }
+    return seqArr[int - 1]
 };
 
 // Do not edit below this line
