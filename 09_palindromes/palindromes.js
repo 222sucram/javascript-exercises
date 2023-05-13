dc1a4598e03e372efac34b0d053ef48ddb26d69b
Marcus <marcus.oflaherty222@gmail.com>
2023-05-13 02:19:14 +0100

    palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strInput = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").split('').filter((el) => el !== ' ')
 
+    const strReverse = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").split('').reverse().filter((el) => el !== ' ')
+    let result = true;
+
+    strInput.forEach((letter, i, arr) => arr[i] === strReverse[i] && result === true ? result = true : result = false)
+    
+    return result
 };
 
 // Do not edit below this line
